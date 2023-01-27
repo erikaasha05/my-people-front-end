6e86e6ec7b62d724b2b799bbe11825bf7c1d3d3a
Erika Sha <erikaasha05@gmail.com>
2023-01-27 14:03:07 -0500

    added new component js files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,12 +9,13 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">My People</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Reminders</Nav.Link>
+            <Nav.Link href="#link">Add Contact</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
