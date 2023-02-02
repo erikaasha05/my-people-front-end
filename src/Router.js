7d2307af942cc87d7f888edca1b8dfeef3f6bb13
Erika Sha <erikaasha05@gmail.com>
2023-02-02 00:37:23 -0500

    created 404 page

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
-import App from "./App"
+import { Navigate } from "react-router-dom";
+import App from "./App";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-
+import FourOhFour from "./pages/404";
 
 export const routes = [
   {
@@ -14,6 +15,14 @@ export const routes = [
   },
   {
     element: <Signup />,
-    path: "/signup"
-  }
+    path: "/signup",
+  },
+  {
+    element: <FourOhFour />,
+    path: "/404",
+  },
+  {
+    path: "*",
+    element: <Navigate to="/404" replace />,
+  },
 ];
